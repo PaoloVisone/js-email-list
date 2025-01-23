5e8bc5c0ebc904107527aeea5421409270face59
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-23 03:25:26 +0100

    [feat] Call Axios (get)

--- script.js
+++ script.js
@@ -3,3 +3,14 @@ const email = document.getElementById('list-email');
 
 // API
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+// Chiamata asincrona
+axios.get(endpoint)
+    .then(function (response) {
+        const result = response.data.response;
+        console.log(result);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
