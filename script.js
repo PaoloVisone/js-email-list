d8ced2fbad1378ae691437e887a01ad4e754b62e
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-23 10:46:28 +0100

    [fix]

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const email = document.getElementById('list-email');
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
 // Variabile d'appoggio
-let iteam = [];
+let iteam = "";
 
 // Ciclo X 10
 for (let i = 0; i <= 10; i++) {
@@ -15,9 +15,9 @@ for (let i = 0; i <= 10; i++) {
         .then(function (response) {
 
             const result = response.data.response;
-            iteam.push(`<li>${result}</li>`);
-            console.log(iteam);
-            email.innerHTML = iteam
+            // iteam.push(`<li>${result}</li>`);
+            // console.log(iteam);
+            email.innerHTML += `<li>${result}</li>`
         })
         .catch(function (error) {
             // handle error
