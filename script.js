64e3ea3f9f14993ad50fec865de24c7e2686ba96
PaoloVisone <polvis.pv.22@gmail.com>
2025-01-24 10:44:53 +0100

    [feat] Event click button [fix]

--- script.js
+++ script.js
@@ -1,26 +1,38 @@
 // Seleziono gli elementi html
 const email = document.getElementById('list-email');
+const btn = document.getElementById('btn');
 
 // API
 const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
 
-// Variabile d'appoggio
-let iteam = "";
+// Evento al click del bottone
+btn.addEventListener('click', () => {
 
-// Ciclo X 10
-for (let i = 0; i <= 10; i++) {
+    // Creo un array per salvare gli elementi
+    let arrMail = [];
 
-    // Chiamata asincrona
-    axios.get(endpoint)
-        .then(function (response) {
+    // Inizializzo l'elemento html vuoto
+    email.innerHTML = "";
 
-            const result = response.data.response;
-            // iteam.push(`<li>${result}</li>`);
-            // console.log(iteam);
-            email.innerHTML += `<li>${result}</li>`
-        })
-        .catch(function (error) {
-            // handle error
-            console.log(error);
-        })
-}
+    // Ciclo X 10
+    for (let i = 0; i <= 10; i++) {
+
+        // Chiamata asincrona
+        axios.get(endpoint)
+            .then(function (response) {
+                // Aquisizione dell'elemento 
+                const result = response.data.response;
+
+                // Push degli elementi per salvarli in un array
+                arrMail.push(result);
+                // console.log(result);
+
+                // Inserimeto con template literals
+                email.innerHTML += `<li>${result}</li>`
+            })
+        // .catch(function (error) {
+        //     // handle error
+        //     console.log(error);
+        // })
+    }
+})
